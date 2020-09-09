a2e758cb3e8d80c25be38d6e2e24892c6eba6119
htmlcssphpjs <63463168+htmlcssphpjs@users.noreply.github.com>
2020-09-09 19:45:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ bot = telebot.TeleBot('1300559351:AAHDX-SwceDVua-M6xymezPgtOCUdwVFfTY')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "/help":
-        bot.send_message(message.from_user.id, "Что не понятно? Пишешь мне сообщение и ждёшь ответа! <a href='https://zen.yandex.ru/media/vsevolodhtml/bot-dlia-obratnoi-sviazi-5f58fe6eefb9584a823adfd8'>Пост обо мне</a>", parse_mode='html')
+        bot.send_message(message.from_user.id, "Что не понятно? Пришешь мне сообщение и ждёшь ответа!")
     elif message.text == "/start":
         bot.send_message(message.chat.id, "Привет, я <i>бот для связи</i> с <b>Всеволодом html</b>, все <u>твои сообщения</u> написаные ниже будут отравляться ему!", parse_mode='html')
     elif message.text == "/id":
         bot.send_message(message.chat.id, message.from_user.id)
     elif message.text == "/go":
     	if message.from_user.id == 1218845111:
-    		bot.send_message(message.chat.id, "ку")
+    		message.text = message.text.replace('/go ', '')
+    		bot.send_message(message.text, "Всеволод html: Привет")
     	else:
     		bot.send_message(message.chat.id, "У вас нету доступа к этой команде!")
     else:
